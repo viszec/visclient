3c04eb825ff35a716421d6131d9d90041958b5b9
mavism <imavisma@gmail.com>
2025-01-28 00:55:02 +1100

    fix Type error: 'framer-motion' has no exported member named 'DynamicAnimationOptions'

--- src/components/ui/vertical-cut-reveal.tsx
+++ src/components/ui/vertical-cut-reveal.tsx
@@ -9,13 +9,13 @@ import {
   useRef,
   useState,
 } from "react"
-import { DynamicAnimationOptions, motion } from "framer-motion"
+import { AnimationOptions, motion } from "framer-motion"
 import { cn } from "@/lib/utils"
 
 interface TextProps {
   children: React.ReactNode
   reverse?: boolean
-  transition?: DynamicAnimationOptions
+  transition?: AnimationOptions
   splitBy?: "words" | "characters" | "lines" | string
   staggerDuration?: number
   staggerFrom?: "first" | "last" | "center" | "random" | number
