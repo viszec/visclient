b57be598bf8101c0b4fad58717a1aa42b378e3d1
mavism <imavisma@gmail.com>
2025-04-22 02:31:27 +1000

    fix: nav padding

--- src/components/common/Navigation.tsx
+++ src/components/common/Navigation.tsx
@@ -10,7 +10,7 @@ export const Navigation = () => (
   <nav className="hidden md:flex items-center">
     {ROUTES.map((item) => (
       <Magnetic key={item.href}>
-        <div className="flex flex-col relative z-[1] p-4 cursor-pointer group">
+        <div className="flex flex-col relative z-[1] pl-8 py-4 cursor-pointer group">
           <Link
             href={item.href}
             className="text-base no-underline hover:text-[#333]/60 text-[#333] font-normal font-halenoir"
@@ -18,7 +18,7 @@ export const Navigation = () => (
             {item.label}
           </Link>
           <div
-            className="absolute w-[5px] h-[5px] top-[38px] left-1/2 
+            className="absolute w-[5px] h-[5px] top-[38px] left-1/2 ml-4
                         bg-gray-700 rounded-full scale-0 -translate-x-1/2 
                         transition-transform duration-200 ease-bezier 
                         group-hover:scale-100"
